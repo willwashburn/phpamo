2d40488182d31e694a7ce5ca59a489f3f893c1b3
Will Washburn <will.washburn@gmail.com>
2015-02-10 23:01:02 -0500

    Use traditional array syntax
    For support for PHP 5.3

--- tests/CamoTest.php
+++ tests/CamoTest.php
@@ -26,13 +26,13 @@ class ProxyUrlTest extends PHPUnit_Framework_TestCase
      */
     public function linksProvider()
     {
-        return [
-            [
+        return array (
+            array(
                 'http://40.media.tumblr.com/4574de09e1207dbb872f9c018adb57c8/tumblr_ngya1hYUBO1rq9ek2o1_1280.jpg',
                 'https://www.example.com/'
-            ],
+            )
 
-        ];
+        );
     }
 
 }
